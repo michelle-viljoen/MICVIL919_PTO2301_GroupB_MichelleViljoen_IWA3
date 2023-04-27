509401ca932077b2934851860421b21dec8fca4b
michelle-viljoen <126607925+michelle-viljoen@users.noreply.github.com>
2023-04-27 15:09:10 +0200

    Add files via upload

---
+++ Challenge2/alex.js
@@ -0,0 +1,8 @@
+ const firstname = "Alex"
+ const surname = "Naidoo"
+ export const role = "Head of Marketing"
+
+ const display= firstname + " " + surname + " (" + role + ")"
+ document.querySelector('#alex').innerText = display
+
+ 
---
+++ Challenge2/nwabisa.js
@@ -0,0 +1,9 @@
+ const firstname = "Nwabisa"
+ const surname = "Gabe"
+ export const role = "CEO"
+
+ const display= firstname + " " + surname + " (" + role + ")"
+ document.querySelector('#nwabisa').innerText = display
+
+ 
+
